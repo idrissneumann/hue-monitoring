8348a72f348d359e931582c6a5f8c7a21dfd2001
Idriss Neumann <idriss.neumann@comwork.io>
2021-11-06 09:22:23 +0100

    Improve on api with the right color

--- api/hue_utils.py
+++ api/hue_utils.py
@@ -6,7 +6,7 @@ import json
 from utils import log_msg
 
 HUE_USERNAME=os.environ['HUE_USERNAME']
-HUE_LIGHTS_COUNT = int(os.environ['HUE_LIGHTS_COUNT'])
+HUE_LIGHTS_COUNT = int(os.environ['HUE_LIGHTS_COUNT']) + 1
 HUE_MONITOR_LIGHTS_IDS=os.environ['HUE_MONITOR_LIGHTS_IDS'].split(",")
 HUE_DISCOVERY_URL = os.environ['HUE_DISCOVERY_URL']
 
@@ -16,6 +16,10 @@ HUE_COLOR_ON=10000
 
 HUE_BRI_OK=20
 HUE_BRI_KO=254
+HUE_BRI_ON=254
+
+HUE_DEFAULT_SAT=254
+HUE_ON_SAT=0
 
 def get_bridge_ip():
   page=requests.get(HUE_DISCOVERY_URL)
@@ -25,19 +29,19 @@ def get_bridge_ip():
   else:
     return ""
 
-def hue_state(on, bri, color, light_id):
-  r = requests.put("http://{}/api/{}/lights/{}/state".format(get_bridge_ip(), HUE_USERNAME, light_id), json = {"on": on, "sat": 254, "bri": bri, "hue": color})
+def hue_state(on, bri, sat, color, light_id):
+  r = requests.put("http://{}/api/{}/lights/{}/state".format(get_bridge_ip(), HUE_USERNAME, light_id), json = {"on": on, "sat": sat, "bri": bri, "hue": color})
   log_msg("DEBUG", "hue_state", r.content)
 
-def hue_states(on, bri, color):
+def hue_states(on, bri, sat, color):
   for i in range(1, HUE_LIGHTS_COUNT):
-    hue_state(on, bri, color, i)
+    hue_state(on, bri, sat, color, i)
 
 def change_color(bri, color, light_id):
-  hue_state(True, bri, color, light_id)
+  hue_state(True, bri, HUE_DEFAULT_SAT, color, light_id)
 
 def switch_off():
-  hue_states(False, 0, 0)
+  hue_states(False, 0, HUE_DEFAULT_SAT, 0)
 
 def switch_on():
-  hue_states(True, HUE_BRI_OK, HUE_COLOR_ON)
+  hue_states(True, HUE_BRI_ON, HUE_ON_SAT, HUE_COLOR_ON)
